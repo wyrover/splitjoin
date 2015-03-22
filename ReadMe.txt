e265c4a41b9a5057b2c9a87173a0241e89658e52
Hirofumi Katayama <katayama.hirofumi.mz@gmail.com>
2015-03-22 14:01:39 +0900

    v0.0.2

--- ReadMe.txt
+++ ReadMe.txt
@@ -1,10 +1,11 @@
 This is katahiromz's C++ split and join.
 
 ---------
-string join(const string_container&, char);
+string joinbychar(const string_container&, char);
 string join(const string_container&, const string&);
-void split(string_container&, const string&, char);
+void splitbychar(string_container&, const string&, char);
 void split(string_container&, const string&, const string&);
+void tokenize(string_container&, const string&, const string&);
 ---------
 
 string_container can be:
@@ -14,3 +15,6 @@ string_container can be:
 	std::list<std::wstring>,
 	std::deque<std::string> or
 	std::deque<std::wstring>.
+
+You can also specify std::set<std::string> or 
+std::unordered_set<std::string> to joinbychar and join.
--- splitjoin.h
+++ splitjoin.h
@@ -1,5 +1,5 @@
 #ifndef KATAHIROMZ_SPLITJOIN
-#define KATAHIROMZ_SPLITJOIN 0x001  // Verison 0.0.1
+#define KATAHIROMZ_SPLITJOIN 0x002  // Verison 0.0.2
 
 ////////////////////////////////////////////////////////////////////////////
 // splitjoin.h --- katahiromz's C++ split and join
@@ -8,11 +8,13 @@
 ////////////////////////////////////////////////////////////////////////////
 // [HISTORY]
 // 2015.03.18 v0.0.1 Created.
+// 2015.03.22 v0.0.2 Added "bychar" and tokenize. Adapted to MSVC.
 ////////////////////////////////////////////////////////////////////////////
-// string join(const string_container&, char);
+// string joinbychar(const string_container&, char);
 // string join(const string_container&, const string&);
-// void split(string_container&, const string&, char);
+// void splitbychar(string_container&, const string&, char);
 // void split(string_container&, const string&, const string&);
+// void tokenize(string_container&, const string&, const string&);
 ////////////////////////////////////////////////////////////////////////////
 
 #ifndef __cplusplus
@@ -25,10 +27,11 @@ namespace KATAHIROMZ_NAMESPACE
 #endif
 
     // string join(const string_container&, char);
-	template <typename t_string_container>
+	template <typename t_string_container,
+	          typename t_string = typename t_string_container::value_type>
 	typename t_string_container::value_type
-	join(const t_string_container& container,
-	     typename t_string_container::value_type::value_type sep)
+	joinbychar(const t_string_container& container,
+	           typename t_string::value_type sep)
 	{
 		typename t_string_container::value_type result;
 		typename t_string_container::const_iterator it = container.begin();
@@ -62,11 +65,12 @@ namespace KATAHIROMZ_NAMESPACE
 		return result;
 	}
 
-	// void split(string_container&, const string&, char);
-	template <typename t_string_container>
-	void split(t_string_container& container,
+	// void splitbychar(string_container&, const string&, char);
+	template <typename t_string_container, 
+	          typename t_string = typename t_string_container::value_type>
+	void splitbychar(t_string_container& container,
 	    const typename t_string_container::value_type& str,
-	    typename t_string_container::value_type::value_type sep)
+		typename t_string::value_type sep)
 	{
 		container.clear();
 		std::size_t i = 0, j = str.find(sep);
@@ -97,11 +101,11 @@ namespace KATAHIROMZ_NAMESPACE
 		std::size_t i = 0, j = str.find(sep);
 #if __cplusplus >= 201103L // modern C++
 		while (j != t_string_container::value_type::npos) {
-			container.push_back(std::move(str.substr(i, j - i)));
+			container.emplace_back(std::move(str.substr(i, j - i)));
 			i = j + 1;
 			j = str.find(sep, i);
 		}
-		container.push_back(std::move(str.substr(i, -1)));
+		container.emplace_back(std::move(str.substr(i, -1)));
 #else // not modern C++
 		while (j != t_string_container::value_type::npos) {
 			container.push_back(str.substr(i, j - i));
@@ -110,6 +114,36 @@ namespace KATAHIROMZ_NAMESPACE
 		}
 		container.push_back(str.substr(i, -1));
 #endif // not modern C++
+	}
+
+	// void tokenize(string_container&, const string&, const string&);
+	template <typename t_string_container>
+	void tokenize(t_string_container& container,
+		const typename t_string_container::value_type& str,
+		const typename t_string_container::value_type& seps)
+	{
+		container.clear();
+		std::size_t i, j;
+		for (i = 0; ;) {
+			j = str.find_first_not_of(seps, i);
+			if (j == t_string_container::value_type::npos) {
+				return;
+			}
+			i = str.find_first_of(seps, j);
+#if __cplusplus >= 201103L // modern C++
+			if (i == t_string_container::value_type::npos) {
+				container.emplace_back(std::move(str.substr(j)));
+				return;
+			}
+			container.emplace_back(std::move(str.substr(j, i - j)));
+#else // not modern C++
+			if (i == t_string_container::value_type::npos) {
+				container.push_back(str.substr(j));
+				return;
+			}
+			container.push_back(str.substr(j, i - j));
+#endif // not modern C++
+		}
 	}
 
 #ifdef KATAHIROMZ_NAMESPACE
